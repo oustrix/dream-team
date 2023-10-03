9d1ecaded6e66921100cc5b841974333a879a1b6
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-03 12:53:20 +0300

    add operation model

--- backend/src/market/api/operations.py
+++ backend/src/market/api/operations.py
@@ -1,10 +1,24 @@
+from typing import List
+
 from fastapi import APIRouter
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from .. import tables
+from ..database import get_session
+from ..models.operations import Operation
 
 router = APIRouter(
     prefix='/operations',
 )
 
 
-@router.get('/')
-def get_operations():
-    return []
+@router.get('/', response_model=List[Operation])
+def get_operations(session: Session = Depends(get_session)):
+    operations = (
+        session
+        .query(tables.Operation)
+        .all()
+    )
+
+    return operations
--- backend/src/market/database.py
+++ backend/src/market/database.py
@@ -13,3 +13,10 @@ Session = sessionmaker(
     autoflush=False,
     autocommit=False,
 )
+
+def get_session() -> Session:
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
