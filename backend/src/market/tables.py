eed64912066e0665ae7fa1c23a444802d1957cb5
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-04 10:46:42 +0300

    add users table

--- backend/src/market/tables.py
+++ backend/src/market/tables.py
@@ -4,6 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    email = sa.Column(sa.Integer, unique=True)
+    username = sa.Column(sa.Text, unique=True)
+    password_hash = sa.Column(sa.Text)
+
+
 class Operation(Base):
     __tablename__ = 'operations'
 
