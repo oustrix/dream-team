5cf4846947d2f2ceb1e70f9c688d7e8a2ea4f79b
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-05 13:42:01 +0300

    create orders service

---
+++ backend/src/market/services/orders.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import List
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from src.market import tables
+from src.market.database import get_session
+from src.market.models.auth import User
+from src.market.models.orders import OrderCreate, OrderStatus
+
+
+class OrdersService:
+    def __init__(self, session: Session = Depends(get_session)):
+        self.session = session
+
+    def get_order(self, order_id: int) -> tables.Order:
+        order = (
+            self.session
+            .query()
+            .filter_by(id=order_id)
+            .first()
+        )
+
+        return order
+
+    # TODO: make filters
+    def get_orders(self) -> List[tables.Order]:
+        orders = (
+            self.session()
+            .query()
+            .all()
+        )
+
+    def create_order(self, order_data: OrderCreate, user_data: User) -> tables.Order:
+        order = tables.Order(**order_data.model_dump())
+        order.owner_id = user_data.id
+        order.status = OrderStatus.OPEN
+        order.created_at = datetime.utcnow()
+
+        self.session.add(order)
+        self.session.commit()
+
+        return order
