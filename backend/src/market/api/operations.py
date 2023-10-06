e805dce1702414585444132f4ed12f5690f6419d
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-06 11:48:23 +0300

    add filter by kind

--- backend/src/market/api/operations.py
+++ backend/src/market/api/operations.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter
 from fastapi import Depends
 
 from ..models.auth import User
-from ..models.operations import Operation, OperationCreate
+from ..models.operations import Operation, OperationCreate, OperationKind
 from ..services.auth import get_current_user
 from ..services.operations import OperationsService
 
@@ -16,10 +16,11 @@ router = APIRouter(
 
 @router.get('/', response_model=List[Operation])
 def get_operations(
+        kind: Optional[OperationKind] = None,
         service: OperationsService = Depends(),
         user: User = Depends(get_current_user)
 ):
-    return service.get_list()
+    return service.get_list(user.id, kind=kind)
 
 
 @router.post('/', response_model=List[Operation])
@@ -28,4 +29,4 @@ def create_operation(
         service: OperationsService = Depends(),
         user: User = Depends(get_current_user)
 ):
-    return service.create(operation_data)
+    return service.create(user.id, operation_data)
--- backend/src/market/services/operations.py
+++ backend/src/market/services/operations.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from ..database import get_session
-from ..models.operations import OperationCreate
+from ..models.operations import OperationCreate, OperationKind
 from .. import tables
 
 
@@ -13,13 +13,14 @@ class OperationsService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def get_list(self, user_id: int) -> List[tables.Operation]:
-        operations = (
-            self.session
-            .query(tables.Operation)
-            .filter_by(user_id=user_id)
-            .all()
-        )
+    def get_list(self, user_id: int, kind: Optional[OperationKind] = None) -> List[tables.Operation]:
+        query = self.session.query(tables.Operation)
+        query.filter_by(user_id)
+
+        if kind:
+            query.filter_by(kind=kind)
+
+        operations = query.all()
 
         return operations
 
