de84d6efe0480f81540ad52e8ce69398c2f479d3
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-06 11:42:24 +0300

    add tags for openapi

--- backend/src/market/app.py
+++ backend/src/market/app.py
@@ -2,5 +2,25 @@ from fastapi import FastAPI
 
 from .api import router
 
-app = FastAPI()
+tags_metadata = [
+    {
+        'name': 'auth',
+        'description': 'Авторизация и регистрация'
+    },
+    {
+        'name': 'operations',
+        'description': 'Операции со счётом'
+    },
+    {
+        'name': 'orders',
+        'description': 'Операции с заказами'
+    }
+]
+
+app = FastAPI(
+    title='Market',
+    description='Биржа фриланса',
+    version='1.0.0',
+    openapi_tags=tags_metadata
+)
 app.include_router(router)
