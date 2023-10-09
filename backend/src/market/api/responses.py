08b363c70b3fefda18e368ade060ca90d1380b37
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-09 15:20:18 +0300

    add create_response route

--- backend/src/market/api/responses.py
+++ backend/src/market/api/responses.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 
 from src.market.models.auth import User
-from src.market.models.responses import Response
+from src.market.models.responses import Response, ResponseCreate
 from src.market.services.auth import get_current_user
 from src.market.services.responses import ResponsesService
 
@@ -30,3 +30,24 @@ def get_responses(
     :return:
     """
     return service.get_responses(order_id, user)
+
+
+@router.post('/', response_model=Response, summary='Создание отклика')
+def create_response(
+        response_data: ResponseCreate,
+        user: User = Depends(get_current_user),
+        service: ResponsesService = Depends()
+):
+    """
+    Создание нового отклика.
+
+    - **order_id**: ID заказа, к которому оставлен отклик.
+    - **description**: комментарий к отклику.
+
+    \f
+    :param response_data:
+    :param user:
+    :param service:
+    :return:
+    """
+    return service.create_response(user, response_data)
