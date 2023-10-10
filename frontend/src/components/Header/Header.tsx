1e6b2b0f787f5dbc1a2e1fec9bc027ff3e51d622
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-10 18:13:27 +0300

    add logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import LOGO from '../../images/logo.svg'
+import styles from '../../styles/Header.module.css'
+import { ROUTES } from '../../utils/routes'
 
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt='TASK HUB' />
+        </Link>
+      </div>
+    </div>
+  )
 }
