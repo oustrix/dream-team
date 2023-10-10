72d1b5baeb4e717fcbae75d8759a076def71019c
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-10 15:41:43 +0300

    add header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import AppRoutes from './components/Routes/Routes';
+import { AppRoutes } from './components/Routes/Routes';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AppRoutes />
+      <Footer />
     </div>
   );
 }
