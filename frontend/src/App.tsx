5fcb4b94c48eed0ee2004a62578bd6899a2eeb68
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-10 15:34:23 +0300

    add AppRoutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppRoutes from './components/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppRoutes />
     </div>
   );
 }
