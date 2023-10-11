bc1119f2419cf2d88f77e41b46fa7cd5d810f61a
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-11 10:42:48 +0300

    add logo to footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import LOGO from '../../images/logo.svg'
+import styles from '../../styles/Footer.module.css'
+
+import { ROUTES } from '../../utils/routes'
 
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <section className={styles.footer}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt='W4F' />
+        </Link>
+      </div>
+    </section>
+  )
 }
