10e22d0d06aada0b6ecf0b27aa77c10d84b2b747
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-11 22:06:31 +0300

    add sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 
+import { NavLink } from 'react-router-dom'
+
+import styles from '../../styles/Sidebar.module.css'
+import { ROUTES } from '../../utils/routes'
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>КАТЕГОРИИ</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li id=''>
+            <NavLink to={`${ROUTES.ORDERS}/?category=`}>Link</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href='/help' target='_blank' className={styles.link}>
+          Помощь
+        </a>
+        <a href='/terms' target='_blank' className={styles.link} style={{ textDecoration: 'underline' }}>
+          Пользовательское соглашение
+        </a>
+      </div>
+    </section>
+  )
 }
