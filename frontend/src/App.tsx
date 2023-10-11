a86ea097cec6a7d1b714fb93b54da538fa046313
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-11 10:44:50 +0300

    add sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { AppRoutes } from './components/Routes/Routes';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <AppRoutes />
+      <Sidebar />
       <Footer />
     </div>
   );
