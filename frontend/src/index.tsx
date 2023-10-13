f9991925fc975e79c417a66088bd2385b9b54beb
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-13 20:29:23 +0300

    add provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
+import { store } from './features/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 )
