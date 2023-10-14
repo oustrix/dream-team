e5ac308e9fde8bf3000fc09ccab3c1c1a20af16f
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 19:11:26 +0300

    add BestWorkers

---
+++ frontend/src/components/BestWorkers/BestWorkers.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from '../../styles/BestWorkers.module.css'
+import { ROUTES } from '../../utils/routes'
+
+export const BestWorkers = ({ workers = [] }) => {
+  return (
+    <section>
+      <h2>Лучшие исполнители</h2>
+      <div className={styles.list}>
+        {workers.map(({ id, image, name, category }) => (
+          <Link to={`${ROUTES.USERS}/${id}`} key={id} className={styles.worker}>
+            <div className={styles.user}>
+              <div className={styles.avatar}>
+                <img src={image} alt={name} />
+              </div>
+
+              <div className={styles.user_info}>
+                <div className={styles.name}>{name}</div>
+                <div className={styles.category}>{category}</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  )
+}
