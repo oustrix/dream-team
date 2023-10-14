246302865d47a77512df6b8b416c8933fe9473c2
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 19:52:05 +0300

    add BestWorkers

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import styles from '../../styles/Home.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { AppDispatch } from '../../features/store'
+import { getWorkers } from '../../features/workers/workersSlice'
+import { BestWorkers } from '../BestWorkers/BestWorkers'
 import { Poster } from '../Poster/Poster'
 import { Sidebar } from '../Sidebar/Sidebar'
 
 export const Home = () => {
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    dispatch(getWorkers())
+  }, [dispatch])
+
+  const workers = useSelector(({ workers }) => workers).list
+
   return (
     <div>
-      <div className={styles.header}>
+      <div style={{ display: 'flex' }}>
         <Sidebar />
         <Poster />
       </div>
+      <BestWorkers workers={workers} amount={8} />
     </div>
   )
 }
