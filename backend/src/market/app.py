6b75e90a6faf58a04864aad04d6765b64518b9bc
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 23:51:16 +0300

    add CORSMiddleware

--- backend/src/market/app.py
+++ backend/src/market/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
 
@@ -23,4 +24,17 @@ app = FastAPI(
     version='1.0.0',
     openapi_tags=tags_metadata
 )
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
