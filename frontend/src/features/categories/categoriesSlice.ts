b6e0eecf80b0ba4c7e5d165c7f9c4e5fa888605a
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 23:56:03 +0300

    add slash to the end fo endpoint

--- frontend/src/features/categories/categoriesSlice.ts
+++ frontend/src/features/categories/categoriesSlice.ts
@@ -5,7 +5,7 @@ import { BASE_URL } from '../../utils/constants'
 
 export const getCategories = createAsyncThunk('categories/getCategories', async (_, thunkAPI) => {
   try {
-    const res = await axios(`${BASE_URL}/categories`)
+    const res = await axios(`${BASE_URL}/categories/`)
     return res.data
   } catch (err) {
     console.log(err)
