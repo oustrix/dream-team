28e75a070489fdfe9e68df9fe85b4f1641120d7a
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 12:28:03 +0300

    add styles

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
+import styles from '../../styles/Home.module.css'
 import { Poster } from '../Poster/Poster'
 import { Sidebar } from '../Sidebar/Sidebar'
 
 export const Home = () => {
   return (
     <div>
-      <div>
+      <div className={styles.header}>
         <Sidebar />
         <Poster />
       </div>
