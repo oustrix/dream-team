c3ea28147ce6ee013ac2509f5f717e5c41c6e276
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 19:19:52 +0300

    add filter for workers

--- frontend/src/components/BestWorkers/BestWorkers.tsx
+++ frontend/src/components/BestWorkers/BestWorkers.tsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 import styles from '../../styles/BestWorkers.module.css'
 import { ROUTES } from '../../utils/routes'
 
-export const BestWorkers = ({ workers = [] }) => {
+export const BestWorkers = ({ workers = [], amount }: { workers: []; amount: number }) => {
+  const list = workers.filter((_, i) => i < amount)
+
   return (
     <section className={styles.workers}>
       <h2>Лучшие исполнители</h2>
       <div className={styles.list}>
-        {workers.map(({ id, image, name, category }) => (
+        {list.map(({ id, image, name, category }) => (
           <Link to={`${ROUTES.USERS}/${id}`} key={id} className={styles.worker}>
             <div className={styles.user}>
               <div className={styles.avatar}>
