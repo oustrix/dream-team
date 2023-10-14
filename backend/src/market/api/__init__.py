51fe46dd4db02ff1a1f41ba43779a4cff307d078
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-14 22:42:53 +0300

    add categories router

--- backend/src/market/api/__init__.py
+++ backend/src/market/api/__init__.py
@@ -4,9 +4,11 @@ from .operations import router as operations_router
 from .auth import router as auth_router
 from .orders import router as orders_router
 from .responses import router as responses_router
+from .categories import router as categories_router
 
 router = APIRouter()
 router.include_router(operations_router)
 router.include_router(auth_router)
 router.include_router(orders_router)
 router.include_router(responses_router)
+router.include_router(categories_router)
