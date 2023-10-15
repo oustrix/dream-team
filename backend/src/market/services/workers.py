dcb77b238b7d2bf4df3fe30d2eaccf10a7170a78
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-15 13:08:10 +0300

    add main category to query

--- backend/src/market/services/workers.py
+++ backend/src/market/services/workers.py
@@ -13,7 +13,10 @@ class WorkersService:
 
     def get_workers(self,
                     amount: int = None) -> List[tables.Worker]:
-        query = self.session.query(tables.Worker)
+        query = (
+            self.session.query(tables.Worker.id, tables.Worker.user_id, tables.Worker.main_category, tables.User.photo)
+            .join(tables.User, tables.Worker.user_id == tables.User.id)
+        )
 
         if amount:
             query.limit(amount)
