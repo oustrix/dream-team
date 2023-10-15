b37f9611d40b9dfcaf14f6d149193380bfec9d3d
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-15 13:38:50 +0300

    add name, surname and category_name fields

--- backend/src/market/models/workers.py
+++ backend/src/market/models/workers.py
@@ -11,4 +11,7 @@ class Worker(WorkerBase):
     id: int
     user_id: int
     main_category: Optional[int]
+    category_name: Optional[str] = None
     photo: Optional[str]
+    name: str
+    surname: str
