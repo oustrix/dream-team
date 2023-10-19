91eb261d6723663ac2a84f05135e4ef3acb300b8
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-19 13:38:56 +0300

    init orders component

--- frontend/src/components/Routes/Routes.tsx
+++ frontend/src/components/Routes/Routes.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { Home } from '../Home/Home'
+import { Orders } from '../Orders/Orders'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path='/orders' element={<Orders />} />
     </Routes>
   )
 }
