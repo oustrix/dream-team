33986c0f2a02dfc1a0055900c0c01e5b1126c7a7
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-20 09:56:00 +0300

    add orders slice

---
+++ frontend/src/features/orders/ordersSlice.ts
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import axios from 'axios'
+
+import { BASE_URL } from '../../utils/constants'
+
+interface OrderRequest {
+  categoryID: number
+  page: number
+}
+
+const getOrders = createAsyncThunk('orders/getOrders', async (ordersRequest: OrderRequest, thunkAPI) => {
+  const { categoryID, page } = ordersRequest
+  const requestURL = new URL(`${BASE_URL}/orders`)
+
+  if (categoryID) {
+    requestURL.searchParams.append('category', categoryID.toString())
+  }
+  if (page) {
+    requestURL.searchParams.append('page', page.toString())
+  }
+
+  try {
+    const res = await axios(requestURL.toString())
+    return res.data
+  } catch (err) {
+    console.log(err)
+    return thunkAPI.rejectWithValue(err)
+  }
+})
+
+const ordersSlice = createSlice({
+  name: 'orders',
+  initialState: {
+    list: [],
+    isLoading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getOrders.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(getOrders.fulfilled, (state, { payload }) => {
+      state.list = payload
+      state.isLoading = false
+    })
+    builder.addCase(getOrders.rejected, (state) => {
+      state.isLoading = false
+    })
+  },
+})
+
+export default ordersSlice.reducer
--- frontend/src/features/store.ts
+++ frontend/src/features/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import categoriesSlice from './categories/categoriesSlice'
 import workersSlice from './workers/workersSlice'
+import ordersSlice from './orders/ordersSlice'
 
 export const store = configureStore({
   reducer: {
     categories: categoriesSlice,
     workers: workersSlice,
+    orders: ordersSlice,
   },
   devTools: true,
 })
