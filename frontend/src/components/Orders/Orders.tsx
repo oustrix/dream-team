9996ec2d742e4db4c2392cfb4984c7e13788377a
oustriX <50222966+oustrix@users.noreply.github.com>
2023-10-25 10:14:06 +0300

    add categories list

--- frontend/src/components/Orders/Orders.tsx
+++ frontend/src/components/Orders/Orders.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 
 import { NavLink, useSearchParams } from 'react-router-dom'
 
+import { getCategories } from '../../features/categories/categoriesSlice'
 import { getOrders, OrderRequest } from '../../features/orders/ordersSlice'
 import { AppDispatch } from '../../features/store'
 import styles from '../../styles/Orders.module.css'
 
 export const Orders = ({ amount }: { amount: number }) => {
   const orders = useSelector(({ orders }) => orders)
+  const categories = useSelector(({ categories }) => categories)
 
   const dispatch = useDispatch<AppDispatch>()
 
@@ -23,7 +25,14 @@ export const Orders = ({ amount }: { amount: number }) => {
       status: 'open',
     }
     dispatch(getOrders(req))
+    dispatch(getCategories())
   }, [dispatch, searchParams, amount])
+
+  const [isCategoriesListVisible, changeCategoriesListVisibility] = useState(false)
+
+  const toggleCategoriesList = () => {
+    changeCategoriesListVisibility(!isCategoriesListVisible)
+  }
 
   return (
     <div>
@@ -49,13 +58,29 @@ export const Orders = ({ amount }: { amount: number }) => {
           <div className={styles.config_categories}>
             <h3 className={styles.config_header}>Категория</h3>
             <div className={styles.select}>
-              <input type='text' placeholder='Выберите категорию' className={styles.input} />
+              <input
+                type='text'
+                placeholder='Выберите категорию'
+                className={styles.input}
+                onClick={toggleCategoriesList}
+              />
               <div className={styles.arrow}>
                 <svg width='16px' height='16px'>
                   <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#arrow`} />
                 </svg>
               </div>
             </div>
+            <ul
+              className={styles.categories_list}
+              id='categories_list'
+              style={{ display: isCategoriesListVisible ? 'block' : 'none' }}
+            >
+              {categories.list.map(({ id, name }: { id: number; name: string }) => (
+                <li key={id} className={styles.category_option} id={id.toString()}>
+                  {name}
+                </li>
+              ))}
+            </ul>
           </div>
           <div className={styles.config_food}>
             <h3 className={styles.config_header}>Способ оплаты</h3>
